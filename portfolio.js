95ac8109ab012bb349420dbcd70571ae6495dcec
Samir Dutta <132768133+samirclasses@users.noreply.github.com>
2023-11-21 18:15:46 +0530

    Add files via upload
    upload files

---
+++ portfolio.js
@@ -0,0 +1,14 @@
+document.write("<h1>Welcome to the Stock Analysis</h1>");
+
+function port1()
+{
+   let icibk=['<h3>STOCK:ICICI BANK LTD</h3> ', '<h3>QUANTITY=10</h3>',  '<h3>AVERAGE PRICE=950</h3>', '<h3>TOTAL COST PRICE=1350</h3>'];
+  document.write('<h2 style="color:blue;">ICICI BANK STOCK HOLDING DETAILS</h2>');
+  document.write(icibk[0]);
+  document.write(icibk[1]);
+  document.write(icibk[2]);
+  document.write(icibk[3]);
+  document.write('<a href="https://www.nseindia.com/get-quotes/equity?symbol=ICICIBANK"><h3 style="color:blue;">STOCK QUOTE</h3></a>');
+  
+  };         
+
